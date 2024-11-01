3fec628c70cf6810e5b027695816066d3bce90d4
andrew robertson <andrewcrobertson@gmail.com>
2024-11-01 11:55:24 +1100

    use omdb in movie reader

--- packages.movies/api/src/services/movieApi/MovieApiImpl.ts
+++ packages.movies/api/src/services/movieApi/MovieApiImpl.ts
@@ -1,38 +1,10 @@
-import { OriginEnum, SourceTypeEnum } from '../common/Movie.types';
-import type { MovieReader, ReadOutput } from './MovieApi.types';
+import { OmdbMovieReader } from '../omdb/OmdbMovieReader';
+import type { MovieReader } from './MovieApi.types';
 
 export class MovieReaderImpl implements MovieReader {
-  public async read(imdbId: string) {
-    // From https://www.omdbapi.com/?i=tt0062622&apikey=<API_KEY>
-    const movie: ReadOutput = {
-      imdbId: 'tt0062622',
-      sourceUrls: ['https://www.omdbapi.com/?i=tt0062622&apikey=<API_KEY>', 'https://some/subtitle/file.srt'],
-      title: '2001: A Space Odyssey',
-      posterUrl: 'https://m.media-amazon.com/images/M/MV5BNjU0NDFkMTQtZWY5OS00MmZhLTg3Y2QtZmJhMzMzMWYyYjc2XkEyXkFqcGc@._V1_SX300.jpg',
-      releaseDate: '1968-05-12T00:00:00.000Z',
-      releaseYear: 1968,
-      rated: 'G',
-      genres: ['Adventure', 'Sci-Fi'],
-      directors: ['Stanley Kubrick'],
-      writers: ['Stanley Kubrick', 'Arthur C. Clarke'],
-      actors: ['Keir Dullea', 'Gary Lockwood', 'William Sylvester'],
-      runTimeMins: 149,
-      plot: 'When a mysterious artifact is uncovered on the Moon, a spacecraft manned by two humans and one supercomputer is sent to Jupiter to find its origins.',
-      subtitlePackages: [
-        {
-          provider: 'HardCoded',
-          author: 'some-author',
-          origin: OriginEnum.Manual,
-          source: {
-            type: SourceTypeEnum.StandaloneFile,
-            sourceUrl: 'https://some/subtitle/file.srt',
-            subtitleFileName: 'file.srt',
-          },
-          text: 'Open the pod bay doors, Hal.',
-        },
-      ],
-    };
+  public constructor(private readonly omdbMovieReader: OmdbMovieReader) {}
 
-    return movie;
+  public async read(imdbId: string) {
+    return await this.omdbMovieReader.read(imdbId);
   }
 }
---
+++ packages.movies/api/src/services/omdb/OmdbMovieReader.ts
@@ -0,0 +1,67 @@
+import { endsWith, map, parseInt, split, trim } from 'lodash-es';
+import { Movie } from '../common/Movie.types';
+import { MovieReader } from '../movieApi/MovieApi.types';
+import { OmdbApi } from './OmdbApi';
+
+export class OmdbMovieReader implements MovieReader {
+  public constructor(private readonly omdbApi: OmdbApi) {}
+
+  public async read(imdbId: string) {
+    const searchRes = await this.omdbApi.search(imdbId);
+    if (!searchRes.success) return { success: false, data: null, logs: searchRes.logs };
+
+    const data = searchRes.data!;
+    const movie: Movie = {
+      imdbId,
+      title: data.Title,
+      posterUrl: this.parseText(data.Poster),
+      releaseDate: this.parseReleaseDate(data.Released),
+      releaseYear: this.parseReleaseYear(data.Year),
+      rated: this.parseText(data.Rated),
+      genres: this.parseTextArray(data.Genre),
+      directors: this.parseTextArray(data.Director),
+      writers: this.parseTextArray(data.Writer),
+      actors: this.parseTextArray(data.Actors),
+      runTimeMins: this.parseRunTime(data.Runtime),
+      plot: this.parseText(data.Plot),
+      subtitlePackages: [],
+    };
+
+    return { success: false, data: movie, logs: searchRes.logs };
+  }
+
+  private parseText(text: string) {
+    // The API seems to return "N/A" in a lot of places, so guard this
+    return text === 'N/A' ? null : text;
+  }
+
+  private parseTextArray(text: string) {
+    // The API seems to return "N/A" in a lot of places, so guard this
+    return text === 'N/A' ? [] : map(split(text, ','), (g) => trim(g));
+  }
+
+  private parseReleaseDate(date: string) {
+    try {
+      return new Date(Date.parse(`${date} UTC`)).toISOString();
+    } catch {
+      return null;
+    }
+  }
+
+  private parseReleaseYear(year: string) {
+    try {
+      return parseInt(year, 10);
+    } catch {
+      return null;
+    }
+  }
+
+  private parseRunTime(runTime: string) {
+    try {
+      if (!endsWith(runTime, 'min')) return null;
+      return parseInt(runTime, 10);
+    } catch {
+      return null;
+    }
+  }
+}
